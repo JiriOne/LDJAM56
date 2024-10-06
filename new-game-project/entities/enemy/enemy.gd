0d565f53735a1434ddb307827158d96d972135c5
Tijn Edens <tijnedens@gmail.com>
2024-10-06 13:23:03 +0200

    player basic attack

--- new-game-project/entities/enemy/enemy.gd
+++ new-game-project/entities/enemy/enemy.gd
@@ -1,17 +1,34 @@
 extends CharacterBody2D
 
 @onready var globalUtil = get_node("/root/GlobalUtil")
+@onready var healthBar = $CanvasGroup/TextureProgressBar
 
 var grid_system
 @export var gridPosition : Vector2 = Vector2.ZERO
+@export var hp = 100
 
 func _ready() -> void:
 	grid_system = get_parent()
 	await grid_system.grid_initialized
 	position = globalUtil.grid_to_world(gridPosition)
 	generator_will_probably_do_this()
+	healthBar.value = hp
 
 func generator_will_probably_do_this():
 	var data = grid_system.get_cell_data(gridPosition)
 	data.has_enemy = true
+	data.enemy = self
 	grid_system.update_cell(data)
+
+func hurt(dp):
+	hp = hp - dp
+	healthBar.value = hp
+	if hp <= 0:
+		print("die")
+		self.die()
+
+func die():
+	var data : GridCellData = grid_system.get_cell_data(gridPosition)
+	data.has_enemy = false
+	grid_system.update_cell(data)
+	queue_free()
